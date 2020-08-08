5098e21f3c954bd6c9f7d614f9072dbe48828ba3
SeanPMulligan <seanmulligan@protonmail.com>
2020-08-08 13:28:02 -0700

    Add initial Screen class

---
+++ Screen.h
@@ -0,0 +1,25 @@
+#pragma once
+#include <SDL2/SDL.h>
+
+class Screen {
+  public:
+    const static int SCREEN_WIDTH = 1200;
+    const static int SCREEN_HEIGHT = 1000;
+  private:
+    SDL_Window *m_window;
+    SDL_Renderer *m_renderer;
+    SDL_Texture *m_texture; 
+    Uint32 *m_buffer1;
+
+  public:
+    int x;
+    int y;
+    Screen();
+    bool init();
+    bool processEvents();
+    void setPixel(int x, int y, Uint8 red, Uint8 green, Uint8 blue);
+    /* void boxBlur(); */
+    void update();
+    void clear();
+    void close();
+};
